e5d13452328b6f9ed3e339453f4a63e4ea5ba52a
CharlineMosquera <char951144@gmail.com>
2023-08-29 15:23:48 -0500

    type-annotated function that returns a float as a str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""type-annotated function that returns
+the string representation of the float"""
+
+
+def to_str(n: float) -> str:
+    """returns a float as a str"""
+    return str(n)
