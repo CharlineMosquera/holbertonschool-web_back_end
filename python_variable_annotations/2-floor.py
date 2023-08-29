8937c4bf2b321a70e22b7a2827a82f3fd53da5df
CharlineMosquera <char951144@gmail.com>
2023-08-29 15:03:14 -0500

    type-annotated function that takes a float number and rounds it down and returns the int

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module containing a type-annotated function that takes a
+float number and rounds it down and returns the integer"""
+
+
+def floor(n: float) -> int:
+    """converts a float number to an integer"""
+    return int(n)
