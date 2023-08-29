40bb42bc05792b69ce6e2750c5674a673a613788
CharlineMosquera <char951144@gmail.com>
2023-08-29 14:27:58 -0500

    type-annotated function type function that concatenates

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""type-annotated function type function that concatenates two str"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concatenates two strings"""
+    return f"{str1}{str2}"
