adeeeda1f4b27fa67373e50b29c818a851ac55f6
CharlineMosquera <char951144@gmail.com>
2023-09-17 18:28:50 -0500

    Getters and Setters

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,8 +1,5 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string' || typeof length !== 'number' || !Array.isArray(students)) {
-      throw new Error('Attribute types are not valid');
-    }
     this._name = name;
     this._length = length;
     this._students = students;
