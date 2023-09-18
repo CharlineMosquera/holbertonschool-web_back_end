88297ce1a395bc71a1c44d3f98af754da4a08885
CharlineMosquera <char951144@gmail.com>
2023-09-17 23:23:07 -0500

    Deletion-resilient hypermedia pagination

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -38,8 +38,8 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        """aaaa
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict: # type: ignore
+        """return a dictionary
         """
         assert isinstance(index, int) and index >= 0
         assert isinstance(page_size, int) and page_size > 0
